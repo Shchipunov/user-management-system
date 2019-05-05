88cab2ffdcc1803eba889d5e6a94788d1b9ef601
svshc <svshc@DESKTOP-8SK7UDH>
2019-05-05 17:36:52 +0300

    Initial commit

---
+++ user-management-system/user-service/src/main/java/com/shchipunov/Application.java
@@ -0,0 +1,13 @@
+package com.shchipunov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] arguments) {
+		SpringApplication.run(Application.class, arguments);
+	}
+
+}
